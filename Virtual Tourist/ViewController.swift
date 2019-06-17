643b6a328540307fd3905feca4eb4e79a7022d60
Ahmad Al-Mutawa <ahmad.mutawa@gmail.com>
2019-06-17 22:35:13 +0300

    Initial Commit

---
+++ Virtual Tourist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Virtual Tourist
+//
+//  Created by Ahmad Al-Mutawa on 6/17/19.
+//  Copyright © 2019 Ahmad Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
