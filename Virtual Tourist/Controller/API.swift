55691ee825867b807fcbc679b43a8649156a9d4b
Ahmad Al-Mutawa <ahmad.mutawa@gmail.com>
2019-06-17 22:45:52 +0300

    Initial commit with setting up the main.storyboard and API

---
+++ Virtual Tourist/Controller/API.swift
@@ -0,0 +1,14 @@
+//
+//  API.swift
+//  Virtual Tourist
+//
+//  Created by Ahmad Al-Mutawa on 6/17/19.
+//  Copyright © 2019 Ahmad Apps. All rights reserved.
+//
+
+import Foundation
+
+class API {
+    private static let key = "03ad452ca7fc8e5b98cda18df0c68d7b"
+    private static let secret = "2c1ac02addba8a2f"
+}
--- Virtual Tourist/Controller/MapViewController.swift
+++ Virtual Tourist/Controller/MapViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MapViewController.swift
 //  Virtual Tourist
 //
 //  Created by Ahmad Al-Mutawa on 6/17/19.
@@ -8,13 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class MapViewController : UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("loaded")
+        
     }
-
-
+    
 }
 
