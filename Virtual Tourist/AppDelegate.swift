0fa7baffe13228524634792bb395ddaaf7a0a3d2
Ahmad Al-Mutawa <ahmad.mutawa@gmail.com>
2019-07-03 20:21:50 +0300

    added comments to the AppDelegate.swift

--- Virtual Tourist/AppDelegate.swift
+++ Virtual Tourist/AppDelegate.swift
@@ -12,12 +12,15 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    
+    // TODO: remove the constant "FlickrModel" to the Constants global object
     let dataController = DataController(modelName: "FlickrModel")
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
         dataController.load ()
+        // injectnig the CoreData container/context to the main view controller
         let nvc = window?.rootViewController as! UINavigationController
         let mvc = nvc.topViewController as! MapViewController
         mvc.dataController = dataController
@@ -28,18 +31,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func applicationDidEnterBackground(_ application: UIApplication) {
+        // app is now in the background. Better save the map location and zoom
         saveDefaults()
-        
-        
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
+        // app is about to be terminated. Better save the map location and zoom
         saveDefaults()
     }
     
     func saveDefaults() {
-        
+        // check if the root view controller IS a navigation controller,
+        // and that its first child is a MapViewController
         if let nvc = window?.rootViewController as? UINavigationController, let mvc = nvc.children[0] as? MapViewController {
+            // call the save method of that controller
             mvc.saveCurrentLocationAndZoom()
             
         }
